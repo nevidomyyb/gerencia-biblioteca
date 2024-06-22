91944570cc9d4040cad4b51bd5a9e45fb2c4c19f
Pedro Cunha <idk.pedroc@gmail.com>
2024-06-22 19:22:09 -0300

    fix: readers definition

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 livros = [
     {"titulo": "O pequeno principe", "autor": "Antoine de Saint-Exupéry", "disponivel": True},
     {"titulo": "Homo Deus: Uma Breve História do Amanhã", "autor": "Yuval Harari", "disponivel": True},
-    {"titulo": "Cálculo I", "autor": "James Stewart", "disponivel": True}
+    {"titulo": "Cálculo I", "autor": "James Stewart", "disponivel": False},
+    {"titulo": "Geometria Analítica", "autor": "Reis e Silva", "disponivel": False},
 ]
-leitores = []
+leitores = [
+    {"nome": "João Carlos da Silva", "livros": [], "multado": False, "lista_multas": []},
+    {"nome": "Nascimento de Oliveira", "livros": [{"título": "Cálculo I", "data_devolucao": "03/07/2024"}], "multado": False, "lista_multas": []},
+    {"nome": "Caio César", "livros": [{"título": "Geometria Analítica", "data_devolucao": "05/06/2024"}], "multado": True, "lista_multas": [{"título": "Geometria Analítica", "valor": 15}]},
+]
 
 def iniciar_aplicativo():
     opcao_escolhida = imprimirMenuPrincipalRetornaOpcao()
