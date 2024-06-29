37fa9a650a8984b38ae8dcff29aeab68ccc94bd5
carol <anakfig2016@gmail.com>
2024-06-29 13:53:31 -0300

    change: relatorio module

--- lib/relatorios.py
+++ lib/relatorios.py
@@ -1,102 +1,77 @@
 from app import livros, leitores
-
-
-def produzirRelatorio():
-    relatorio = []
-
-    for livro in livros:
-        if livro["disponivel"] == True:
-
-            item = {
-                "titulo": livro["titulo"],
-                "disponivel": True,
-                "multado": False,
-            }
-            relatorio.append(item)
-
-        else:
-            for leitor in leitores:
-                if len(leitor["lista_multas"]) != 0:
-                    for livro_indisponivel in leitor["lista_multas"]:
-                        if livro_indisponivel["titulo"] == livro["titulo"]:
-                            item = {
-                                "titulo": livro["titulo"],
-                                "disponivel": False,
-                                "multado": True,
-                                "leitor": leitor["nome"]
-                            }
-                            relatorio.append(item)
-                        else:
-                            item = {
-                                "titulo": livro["titulo"],
-                                "disponivel": False,
-                                "multado": False,
-                            }
-                            relatorio.append(item)
-
-    return relatorio
+from datetime import date
 
 
 def imprimirRelatorio():
-    relatorio = produzirRelatorio()
 
-    lista = []
+    lista_disponiveis = []
+    lista_emprestados = []
+    lista_multados = []
 
-    for livro in relatorio:
-        lista.append(livro["titulo"])
-
-    maior_disponivel = len(max(lista, key=len))
-
-    print("-"*(maior_disponivel+8))
-    print("| ", end="")
-    print("Disponiveis".center(maior_disponivel+4), end="")
-    print(" |")
-
-    indiceDisponiveis = 0
-    for livro in relatorio:
-
+    for livro in livros:
         if livro["disponivel"] == True:
-            indiceDisponiveis +=1
-            print("| ", end="")
-            print(f"{indiceDisponiveis} - ", end="")
-            print(f"{livro["titulo"]}".ljust(maior_disponivel), end="")
-            print(" |")
+            lista_disponiveis.append(livro["titulo"])
+        else:
+            for leitor in leitores:
+                if len(leitor["lista_multas"]) != 0:
+                    for livro_multado in leitor["lista_multas"]:
+                        if livro["titulo"] == livro_multado["titulo"]:
+                            lista_emprestados.append(livro["titulo"])
+                            lista_multados.append(leitor["nome"])
+                        else:
+                            lista_emprestados.append(livro["titulo"])
 
-    print("-"*(maior_disponivel+8))
-    print()
-    
+    lista_disponiveis.sort(key=len, reverse=True)
+    lista_emprestados.sort(key=len, reverse=True)
+    lista_multados.sort(key=len, reverse=True)
 
-    print("-"*(maior_disponivel+8))
-    print("| ", end="")
-    print("Emprestados".center(maior_disponivel+4), end="")
+    maior_disponivel = len(max(lista_disponiveis, key=len))
+    maior_emprestado = len(max(lista_emprestados, key=len))
+    maior_multado = len(max(lista_multados, key=len))
+
+    tamanho_maximo = max(len(lista_disponiveis), len(
+        lista_emprestados), len(lista_multados))
+    largura_maxima = (maior_disponivel + maior_emprestado + maior_multado + 12)
+
+    data = date.today()
+    data_string = f"Relatório do dia {data.strftime('%d/%m/%Y')}"
+
+    print(data_string.rjust(52))
+    print('-'*largura_maxima)
+    print(" | ", end="")
+    print("Disponíveis".center(maior_disponivel), end="")
+    print(" | ", end="")
+    print("Emprestados".center(maior_emprestado), end="")
+    print(" | ", end="")
+    print("Multados".center(maior_multado), end="")
     print(" |")
-
-    indiceEmprestados = 0
-    for livro in relatorio:
-
-        if livro["disponivel"] == False:
-            indiceEmprestados += 1
-            print("| ", end="")
-            print(f"{indiceEmprestados} - ", end="")
-            print(f"{livro["titulo"]}".ljust(maior_disponivel), end="")
-            print(" |")
-
-    print("-"*(maior_disponivel+8))
+    for indice in range(tamanho_maximo):
+        if indice < len(lista_disponiveis):
+            print(" | ", end="")
+            print(lista_disponiveis[indice].ljust(maior_disponivel), end="")
+            print(" | ", end="")
+        else:
+            print("".ljust(maior_disponivel), end="")
+            print(" | ", end=" ")
+        if indice < len(lista_emprestados):
+            print(lista_emprestados[indice].ljust(maior_emprestado), end="")
+            print(" | ", end="")
+        else:
+            print("".ljust(maior_emprestado), end="")
+            print(" | ", end="")
+        if indice < len(lista_multados):
+            print(lista_multados[indice].ljust(maior_multado), end="")
+            print(" | ")
+        else:
+            print("".ljust(maior_multado), end="")
+            print(" | ")
+    print('-'*largura_maxima)
+    print(" | ", end="")
+    print(f"Total: {len(lista_disponiveis)}".ljust(maior_disponivel), end="")
+    print(" | ", end="")
+    print(f"Total: {len(lista_emprestados)}".ljust(maior_emprestado), end="")
+    print(" | ", end="")
+    print(f"Total: {len(lista_multados)}".ljust(maior_multado), end="")
+    print(" | ")
     print()
-    
-
-    print("-"*(maior_disponivel+8))
-    print("| ", end="")
-    print("Multados".center(maior_disponivel+4), end="")
-    print(" |")
-    indiceMultados = 0
-    for livro in relatorio:
-
-        if livro["multado"] == True:
-            indiceMultados += 1
-            print("| ", end="")
-            print(f"{indiceMultados} - ", end="")
-            print(f"{livro["leitor"]}".ljust(maior_disponivel), end="")
-            print(" |")
-    print("-"*(maior_disponivel+8))
     print()
