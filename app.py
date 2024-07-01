a72e122d35f7553b732cc66bad7b2ee7bb277dc7
carol <anakfig2016@gmail.com>
2024-07-01 07:40:57 -0300

    fix: readers list

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 
 livros = [
-    {"titulo": "O pequeno principe", "autor": "Antoine de Saint-Exupéry", "disponivel": True},
-    {"titulo": "Homo Deus: Uma Breve História do Amanhã", "autor": "Yuval Harari", "disponivel": True},
+    {"titulo": "O pequeno principe",
+        "autor": "Antoine de Saint-Exupéry", "disponivel": True},
+    {"titulo": "Homo Deus: Uma Breve História do Amanhã",
+        "autor": "Yuval Harari", "disponivel": True},
     {"titulo": "Cálculo I", "autor": "James Stewart", "disponivel": False},
-    {"titulo": "Geometria Analítica", "autor": "Reis e Silva", "disponivel": False},    
-    {"titulo": "A cor que caiu do espaço", "autor": "H. P. Lovecraft", "disponivel": True},
+    {"titulo": "Geometria Analítica", "autor": "Reis e Silva", "disponivel": False},
+    {"titulo": "A cor que caiu do espaço",
+        "autor": "H. P. Lovecraft", "disponivel": True},
     {"titulo": "O Chamado de Cthulhu", "autor": "H. P. Lovecraft", "disponivel": True},
 ]
 leitores = [
-    {"nome": "João Carlos da Silva", "livros": [], "multado": False, "lista_multas": []},
-    {"nome": "Nascimento de Oliveira", "livros": [{"titulo": "Cálculo I", "data_devolucao": "09/07/2024", "data_emprestimo": "20/06/2024"}], "multado": False, "lista_multas": []},
-    {"nome": "Caio César", "livros": [{"título": "Geometria Analítica", "data_devolucao": "04/06/2024", "data_emprestimo": "26/05/2024"}], "multado": True, "lista_multas": [{"titulo": "Geometria Analítica", "valor": 15}, {"titulo": "Biblia", "valor": 15}]},
+    {"nome": "João Carlos da Silva", "livros": [],
+        "multado": False, "lista_multas": []},
+    {"nome": "Nascimento de Oliveira", "livros": [
+        {"titulo": "Cálculo I", "data_devolucao": "09/07/2024", "data_emprestimo": "20/06/2024"}], "multado": False, "lista_multas": []},
+    {"nome": "Caio César", "livros": [{"título": "Geometria Analítica", "data_devolucao": "04/06/2024",
+                                       "data_emprestimo": "26/05/2024"}], "multado": True, "lista_multas": [{"titulo": "Geometria Analítica", "valor": 15}]},
 ]
+
 
 def iniciar_aplicativo():
     opcao_escolhida = imprimirMenuPrincipalRetornaOpcao()
@@ -27,8 +34,7 @@ def iniciar_aplicativo():
                 imprimirAjuda()
             case _:
                 print('[!] Opção Inválida.')
-            
-        
+
         opcao_escolhida = imprimirMenuPrincipalRetornaOpcao()
 
 
